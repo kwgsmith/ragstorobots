eccb3412c85e9e78d25ff00134e34c347cbb5a4d
Kenneth Smith <kwgsmith@gmail.com>
2015-06-02 10:42:08 -0500

    Started UI scripts

---
+++ Rags to Robots/Assets/Scripts/UI/DragPanel.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+
+public class DragPanel : MonoBehaviour, IPointerDownHandler, IDragHandler {
+
+	private Vector2 pointerOffset;
+	private RectTransform canvasRectTransform;
+	private RectTransform panelRectTransform;
+
+	void Awake()
+	{
+		Canvas canvas = GetComponentInParent<Canvas> ();
+		if (canvas != null) 
+		{
+			canvasRectTransform = canvas.transform as RectTransform;
+			panelRectTransform = transform as RectTransform;
+		}
+	}
+
+}
