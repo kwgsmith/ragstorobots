72f8b4eec7711b1b433cbc697851cd081fb59000
Kenneth Smith <kwgsmith@gmail.com>
2015-06-14 16:17:20 -0500

    Editor script changes

---
+++ Rags to Robots/Assets/Crafting System/Scripts/CSRecipe.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using WarpwareStudios.ItemSystem;
+
+namespace WarpwareStudios.CraftingSystem
+{
+	public class CSRecipe : ICSRecipe
+	{
+		ISObject _item;
+		Dictionary<ISObject, int> _partsList = new Dictionary<ISObject,int>();
+
+		#region ICSRecipe implementation
+
+		public ISObject Item { get { return _item; } }
+		public Dictionary<ISObject, int> partsList { get { return _partsList; } }
+
+		#endregion
+	}
+}
---
+++ Rags to Robots/Assets/Crafting System/Scripts/Interfaces/ICSRecipe.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+using WarpwareStudios.ItemSystem;
+
+namespace WarpwareStudios.CraftingSystem
+{
+	//base recipe
+	public interface ICSRecipe
+	{
+		//item to create
+		ISObject Item { get; }
+		//objects needed to create this item
+		Dictionary<ISObject,int> partsList { get; }
+	}
+}
